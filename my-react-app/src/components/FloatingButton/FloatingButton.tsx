5ea7e13fb340cc230b7046376841175262f27dcd
Dawid Gałecki <dgalecki@concisesoftware.com>
2023-03-21 11:47:12 +0100

    Add animations

--- my-react-app/src/components/FloatingButton/FloatingButton.tsx
+++ my-react-app/src/components/FloatingButton/FloatingButton.tsx
@@ -4,7 +4,7 @@ export function FloatingButton() {
   return (
     <div className="floatingButton__container">
       <div className="floatingButton__button">
-        <span>&oplus;</span>
+        <span className="floatingButton__icon">&oplus;</span>
       </div>
     </div>
   );
